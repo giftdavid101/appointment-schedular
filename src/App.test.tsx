6fc345135d7e4ad19a480cb47cf00251ac6983a1
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 <gift@rewriteagency.com>
2021-09-29 13:13:51 +0100

    initial setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Ankora front end test/i);
   expect(linkElement).toBeInTheDocument();
 });
